775405541f07c06817cdd4b7158e38c257df17b3
matchubelo <matthew.belo@icloud.com>
2025-01-23 11:36:19 -0500

    Code written on 01/23/25

---
+++ Python/Calculations.py
@@ -0,0 +1,10 @@
+print("This is a project that adds the two numbers defined by the user.")
+print("")
+
+N1 = input("Enter a number: ")
+print("")
+
+N2 = input("Enter the another number: ")
+print("")
+
+print(N1, "+", N2, "=", int(N1) + int(N2))
