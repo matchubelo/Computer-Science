abb8aea2f5043bbd47de21cd13f9d952a071ee3e
matchubelo <matthew.belo@icloud.com>
2025-09-04 18:45:31 -0400

    Sales and Star Projects2

---
+++ Java/SalesPredictionMJB.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class SalesPredictionMJB 
+{
+    public static void main(String[] args) 
+    {
+      Integer totalsales =  8300000;
+      Double eastcoastdivisionsales = (totalsales * 0.65);
+      System.out.println("The total sals for the east coast division will be: $" + eastcoastdivisionsales);
+    }
+}
---
+++ Java/StarPatternMJB.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class StarPatternMJB 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+        System.out.println(" ***** ");
+        System.out.println("  ***  ");
+        System.out.println("   *   ");
+    } 
+}
