4e1d2083b9ff3642932a0217d6ed6a97fa4e307e
matchubelo <matthew.belo@icloud.com>
2025-09-29 19:49:49 -0400

    fixed driver program for payroll assignment for

--- Java/PayrollProgramMJB.java
+++ Java/PayrollProgramMJB.java
@@ -7,25 +7,64 @@ public class PayrollProgramMJB
     {
         Scanner scanner = new Scanner(System.in);
         PayrollClassesMJB pay = new PayrollClassesMJB();
+        PayrollClassesMJB pay2 = new PayrollClassesMJB();
+
+
+        // EMPLOYEE 1:
+
 
         System.out.println("What is your name?");
         pay.setname(scanner.nextLine());
 
         System.out.println("What is your ID Number?");
         pay.setidnumber(scanner.nextInt());
-        
+        scanner.nextLine();
+
         System.out.println("What is your hourly rate?");
         pay.sethourlyrate(scanner.nextDouble());
+
 
         System.out.println("How many hours did you work?");
         pay.sethours(scanner.nextDouble());
 
+
         System.out.println("");
         System.out.println("Employee: " + pay.getname());
         System.out.println("Employee ID: " + pay.getidnumber());
-        System.out.println("");
+
         System.out.println(pay.getname() + "'s Gross Pay for " + pay.gethours() + "HRs of work at a rate of $" + pay.gethours() + "/HR is $" + String.format("%.2f", pay.getgrosspay()));
 
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+
+
+
+        // EMPLOYEE 2:
+        
+
+        System.out.println("What is your name?");
+        scanner.nextLine();
+        pay2.setname(scanner.nextLine());
+
+        System.out.println("What is your ID Number?");
+        pay2.setidnumber(scanner.nextInt());
+        scanner.nextLine();
+
+        System.out.println("What is your hourly rate?");
+        pay2.sethourlyrate(scanner.nextDouble());
+
+
+        System.out.println("How many hours did you work?");
+        pay2.sethours(scanner.nextDouble());
+
+
+        System.out.println("");
+        System.out.println("Employee: " + pay2.getname());
+        System.out.println("Employee ID: " + pay2.getidnumber());
+        System.out.println("");
+        System.out.println(pay2.getname() + "'s Gross Pay for " + pay2.gethours() + "HRs of work at a rate of $" + pay2.gethours() + "/HR is $" + String.format("%.2f", pay2.getgrosspay()));
 
         scanner.close();
     }
